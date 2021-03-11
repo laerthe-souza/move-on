e34984ac7457e9eac8422c3e6a64ef14b5055d26
Laerthe Souza <laerte_souza.developer@outlook.com>
2021-03-11 16:55:31 -0300

    Adding meta tags to SEO

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -41,9 +41,30 @@ export default class MyDocument extends Document {
       <Html lang="pt-br">
         <Head>
           <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
+          <meta httpEquiv="X-UA-Compatible" content="ie=edge, chrome=1" />
+
+          <meta name="author" content="Laerthe Souza" />
+          <meta
+            name="description"
+            content="Um aplicativo baseado na Técnica Pomodoro, para aqueles que desejam um aumento na produtividade e maior foco."
+          />
+          <meta
+            name="keywords"
+            content="tecnica pomodoro, pomodoro, produtividade, programacao"
+          />
+
+          <meta property="og:title" content="Técnica Pomodoro | move.on" />
+          <meta
+            property="og:image"
+            content="https://i.ibb.co/KzqcskR/Login.jpg"
+          />
+          <meta
+            property="og:description"
+            content="Quer ter maior produtivide e foco? Venha conhecer o move.on"
+          />
+          <meta property="og:url" content="//www.example.com/URL do artigo" />
 
           <link rel="shortcut icon" href="/favicon.png" type="image/png" />
-
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap"
